297fbaf3a23e1ea6b443a9700fff68ccf551b199
Tansy Arron <tansy.arron@gmail.com>
2015-01-10 19:27:44 -0500

    Cleaned up get_request()

--- get_request.py
+++ get_request.py
@@ -1,48 +1,51 @@
+import sys
 import urllib.request
 import urllib.parse
 import bdecoder as bdecoder
 import hashlib as hashlib
+import myclient as myclient
 
 '''
 Generate working url request
-It should look like:
-http://thomasballinger.com:6969/announce?uploaded=0&compact=1&info_hash=%2B%15%CA%2B%FDH%CD%D7m9%ECU%A3%AB%1B%8AW%18%0A%09&event=started&downloaded=0&peer_id=1406230005.05tom+cli&port=6881&left=1277987
 '''
+
 def get_peer_info():
-	with open('flagfromserver.torrent', 'rb') as f:
-	# sha1 hash bencoded info dictionary
-	# url encoded hash should match: %2B%15%CA%2B%FDH%CD%D7m9%ECU%A3%AB%1B%8AW%18%0A%09
-		dictionary = bdecoder.bdecoder(f.read())
-		info = dictionary[b'info']
-		ben_string = info['ben_string']
-		info_hash = hashlib.sha1(ben_string)
-		params = {
-			'info_hash' : '%2B%15%CA%2B%FDH%CD%D7m9%ECU%A3%AB%1B%8AW%18%0A%09',
-			'event' : "started",	# event- default "started"
-			'downloaded' : '0',		# number of bytes downloaded
-			'peer_id' : '-TZ-0000-00000000000',
-			'port' : '6881',
-			'left' : info[b'length'].decode('utf-8'),	# number of bytes left to download, default number of bytes in file
-			}
+	meta_info_file = sys.argv[1]
+	with open(meta_info_file, 'rb') as f:
+		meta_info_dict = bdecoder.bdecoder(f.read())
+	return meta_info_dict
 
-		tracker_url = dictionary[b'announce'].decode('utf-8') + '?'
-		url_elems = []
-		for key in params:
-			url_elems.append(key + '=' + params[key])
-		url_add = '&'.join(url_elems)	
-		tracker_url += url_add
-		
-		return bdecoder.bdecoder(urllib.request.urlopen(tracker_url).read())
+def create_tracker_query(meta_info_dict):
+	info = meta_info_dict[b'info']
+	ben_string = info['ben_string']
+	info_hash = hashlib.sha1(ben_string).digest()
+	params = {
+		'info_hash' : info_hash,
+		'event' : "started",	# event- default "started"
+		'downloaded' : '0',		# number of bytes downloaded
+		'peer_id' : '-TZ-0000-00000000000',
+		'port' : '6881',
+		'left' : info[b'length'],	# number of bytes left to download, default number of bytes in file
+	}
+	tracker_url = meta_info_dict[b'announce'].decode('utf-8')
+	query = urllib.parse.urlencode(params, doseq=False, safe='', encoding=None, errors=None)
+	return '{}?{}'.format(meta_info_dict[b'announce'].decode('utf-8'), query)
 
-peer_info = get_peer_info()
-raw_peer_info = [i for i in peer_info[b'peers']]
-peers = [raw_peer_info[i:i+6] for i in range(0,len(raw_peer_info), 6)]
-for peer in peers:
-	for i in range(4):
-		 peer[i] = str(peer[i])
-peer_dict = {('.'.join(peer[:4])):peer[4]*256+peer[5] for peer in peers}
-print(peer_dict)
+def create_peer_dict(raw_peer_info):
+	peer_info = raw_peer_info[b'peers']
+	peers = [peer_info[i:i+6] for i in range(0, len(peer_info), 6)]
+	peer_dict = {
+		'.'.join(str(i) for i in peer[:4]): int.from_bytes(peer[4:], byteorder='big')
+		for peer in peers
+	}
+	return peer_dict
 
+meta_info_dict = get_peer_info()
+tracker_query = create_tracker_query(meta_info_dict)
+raw_peer_info = bdecoder.bdecoder(urllib.request.urlopen(tracker_query).read())
+peer_dict = create_peer_dict(raw_peer_info)
+for key, value in peer_dict.items():
+	myclient.create_client_socket(key, value)
 
 
 
--- myclient.py
+++ myclient.py
@@ -1,29 +1,30 @@
 import socket
 import sys
+def create_client_socket(IP, port):
+	# Create a socket
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Create a socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	# Connect the socket to the port where the server is listening
+	server_address = (IP, port)
+	print(server_address)
+	print('connecting to %s port %s' % server_address, file=sys.stderr)
+	sock.connect(server_address)
 
-# Connect the socket to the port where the server is listening
-server_address = ('96.126.104.219', 63103)
-print('connecting to %s port %s' % server_address, file=sys.stderr)
-sock.connect(server_address)
+	try:
+		# send data
+		message = 'This is the message. It will be repeated'
+		print('sending "%s"' % message, file=sys.stderr)
+		sock.sendall(message.encode('utf-8'))
 
-try:
-	# send data
-	message = 'This is the message. It will be repeated'
-	print('sending "%s"' % message, file=sys.stderr)
-	sock.sendall(message.encode('utf-8'))
+		# look for the response
+		amount_received = 0
+		amount_expected = len(message)
 
-	# look for the response
-	ammount_received = 0
-	ammount_expected = len(message)
-
-	while ammount_received < ammount_expected:
+		# while amount_received < amount_expected:
 		data = sock.recv(16)
-		ammount_received += len(data)
-		print('received "%s"' % data.decode('utf-8'), file=sys.stderr)
+		amount_received += len(data)
+		print('received "%s"' % len(data))#data.decode('utf-8'), file=sys.stderr)
 
-finally:
-	print('closing socket', sys.stderr)
-	sock.close()
+	finally:
+		print('closing socket', sys.stderr)
+		sock.close()
